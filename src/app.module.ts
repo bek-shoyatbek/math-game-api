b4c0a8aa51e1195363e4bd3178f19ac585809b97
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-12-06 20:36:12 +0500

    fail: no security

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from './auth/auth.guard';
 import { RatingsModule } from './ratings/ratings.module';
 
 @Module({
   imports: [AuthModule, UsersModule, RatingsModule],
   controllers: [AppController],
-  providers: [AppService, { provide: APP_GUARD, useClass: AuthGuard }],
+  providers: [AppService],
 })
 export class AppModule {}
