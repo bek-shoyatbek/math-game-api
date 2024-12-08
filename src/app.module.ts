7c87ef54766cd8f3bf8158202dcf7a3370e4caf3
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-12-08 23:17:07 +0500

    feat: custom http logger fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { RatingsModule } from './ratings/ratings.module';
+import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {AuthModule} from './auth/auth.module';
+import {UsersModule} from './users/users.module';
+import {RatingsModule} from './ratings/ratings.module';
+import {HttpLoggerMiddleware} from "./middlewares/http-logger.middleware";
 
 @Module({
-  imports: [AuthModule, UsersModule, RatingsModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [AuthModule, UsersModule, RatingsModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer): any {
+        consumer.apply(HttpLoggerMiddleware)
+            .forRoutes('*');
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as morgan from 'morgan';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: '*',
-  });
+    app.enableCors({
+        origin: '*',
+    });
 
 
-  app.use(morgan('dev'));
-
-  await app.listen(3030);
+    await app.listen(3030);
 }
+
 bootstrap();
