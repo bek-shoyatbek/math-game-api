46b0e253e0621df156f2fbc449aafd5d6dfe92d9
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-12-08 23:20:09 +0500

    fix: /users/all -> /users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,51 +1,52 @@
 import {
-  BadRequestException,
-  Body,
-  Controller,
-  Get,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Query,
-  UseFilters,
+    BadRequestException,
+    Body,
+    Controller,
+    Get,
+    Param,
+    ParseIntPipe,
+    Patch,
+    Query,
+    UseFilters,
 } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { Prisma } from '@prisma/client';
-import { PrismaClientExceptionFilter } from 'src/prisma-client-exception/prisma-client-exception.filter';
-import { ObjectId } from 'mongodb';
+import {UsersService} from './users.service';
+import {Prisma} from '@prisma/client';
+import {PrismaClientExceptionFilter} from 'src/prisma-client-exception/prisma-client-exception.filter';
+import {ObjectId} from 'mongodb';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
-
-  @Get('all')
-  @UseFilters(PrismaClientExceptionFilter)
-  async getUsers(
-    @Query('order', new ParseIntPipe({ optional: true })) order?: number,
-    @Query('limit', new ParseIntPipe({ optional: true })) limit?: number,
-  ) {
-    return this.usersService.getUsers(order, limit);
-  }
-
-  @Get('/:id')
-  @UseFilters(PrismaClientExceptionFilter)
-  async getUser(@Param('id') id: string) {
-    return this.usersService.getUser(id);
-  }
-
-  @Patch('/:id')
-  @UseFilters(PrismaClientExceptionFilter)
-  async updateUser(
-    @Param('id') id: string,
-    @Body() updateUserDto: Prisma.UserUpdateInput,
-  ) {
-    if (!ObjectId.isValid(id)) {
-      throw new BadRequestException('Invalid user id');
-    }
-    if (updateUserDto.email) {
-      throw new BadRequestException('Email cannot be changed');
+    constructor(private readonly usersService: UsersService) {
     }
 
-    return this.usersService.updateUser(id, updateUserDto);
-  }
+    @Get('')
+    @UseFilters(PrismaClientExceptionFilter)
+    async getUsers(
+        @Query('order', new ParseIntPipe({optional: true})) order?: number,
+        @Query('limit', new ParseIntPipe({optional: true})) limit?: number,
+    ) {
+        return this.usersService.getUsers(order, limit);
+    }
+
+    @Get('/:id')
+    @UseFilters(PrismaClientExceptionFilter)
+    async getUser(@Param('id') id: string) {
+        return this.usersService.getUser(id);
+    }
+
+    @Patch('/:id')
+    @UseFilters(PrismaClientExceptionFilter)
+    async updateUser(
+        @Param('id') id: string,
+        @Body() updateUserDto: Prisma.UserUpdateInput,
+    ) {
+        if (!ObjectId.isValid(id)) {
+            throw new BadRequestException('Invalid user id');
+        }
+        if (updateUserDto.email) {
+            throw new BadRequestException('Email cannot be changed');
+        }
+
+        return this.usersService.updateUser(id, updateUserDto);
+    }
 }
