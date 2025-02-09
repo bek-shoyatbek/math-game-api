22607fb2799b03fb7c4877855995123d0e8604dc
javohir <example@gmail.com>
2025-02-09 15:53:41 +0000

    fix: application port logging added

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const configService = app.get<ConfigService>(ConfigService);
+
+    const appPort = configService.get<number>("PORT") ?? 4000;
 
     app.enableCors({
         origin: '*',
     });
 
 
-    await app.listen(3030);
+    console.log("Application is running on port: ", appPort); 
+    await app.listen(appPort);
 }
 
 bootstrap();
